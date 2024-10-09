4929017c3294134a230370b1fc27b24f09e1fff6
FuneTome <kurilenkof@gmail.com>
2024-10-09 11:03:36 +0300

    Дополнительное задание

--- Lioa/Dop_task.cpp
+++ Lioa/Dop_task.cpp
@@ -27,15 +27,23 @@ int main() {
 			}
 		cout << '\n';
 	}
-
-	cout << '\n';
 	int size = 0;
 	for (int i = 0; i < len; i++) {
 		for (int j = i; j < len; j++) {
 			size += G[i][j];
-			if (G[i][i] == 0 && G[i][j] == 1) size--;
 		}
 	}
+	cout << "\nРазмер графа равен: " << size << endl;
+	for (int i = 0; i < len; i++) {
+		int sum = 0;
+		for (int j = 0; j < len; j++) {
+			sum += G[i][j];
+		}
+		if ((sum == (len - 1) && G[i][i] == 0) || (sum == len)) cout << "Вершина " << i + 1 << " является доминирующей\n";
+		else if (sum == 1 && G[i][i] == 0) cout << "Вершина " << i + 1 << " является концевой\n";
+		else if ((sum == 1 && G[i][i] == 1) || (sum == 0)) cout << "Вершина " << i + 1 << " является изолированной\n";
+	}
+	cout << '\n';
 	g = new int*[len];
 	for (int i = 0; i < len; i++) {
 		g[i] = new int[size];
@@ -48,13 +56,15 @@ int main() {
 	int sum = 0;
 	for (int i = 0; i < len; i++) {
 		for (int j = i; j < len; j++) {
-			if (G[i][i] == 0) { continue; }
+			if (G[i][j] == 1 && i == j) {
+				g[i][sum] = 2;
+				sum++;
+			}
 			else if (G[i][j] == 1) {
 				g[i][sum] = 1;
 				g[j][sum] = 1;
 				sum++;
 			}
-
 		}
 	}
 	cout << "Матрица инцидентвности: \n\n";
@@ -63,6 +73,17 @@ int main() {
 			cout << g[i][j] << " ";
 		}
 		cout << '\n';
+	}
+	cout << "\nРазмер графа равен: " << size << endl;
+	for (int i = 0; i < len; i++) {
+		int sum = 0;
+		for (int j = 0; j < size; j++) {
+			sum += g[i][j];
+			if (g[i][j] == 2) sum-=2;
+		}
+		if (sum >= (len - 1)) cout << "Вершина " << i + 1 << " является доминирующей\n";
+		else if (sum == 1) cout << "Вершина " << i + 1 << " является концевой\n";
+		else if (sum == 0) cout << "Вершина " << i + 1 << " является изолированной\n";
 	}
 	return 0;
 }
