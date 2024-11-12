1f748f464300a702875546c83763a0904bdb1ff7
FuneTome <kurilenkof@gmail.com>
2024-11-12 20:17:57 +0300

    Задание 1

--- Project/task1.cpp
+++ Project/task1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <limits>
 using namespace std;
 
 struct node {
@@ -55,6 +56,25 @@ int** creatG(int len) {
 	return G;
 }
 
+int** creatG_orint(int len) {
+	int** G;
+	srand(time(0));
+	G = new int* [len];
+	for (int i = 0; i < len; i++) {
+		G[i] = new int[len];
+	}
+	for (int i = 0; i < len; i++) {
+		for (int j = i; j < len; j++) {
+			if (i == j) G[i][i] = 0;
+			else {
+				G[i][j] = rand() % 10;
+				G[j][i] = G[i][j];
+			}
+		}
+	}
+	return G;
+}
+
 void printG(int** G, int len) {
 	for (int i = 0; i < len; i++) {
 		for (int j = 0; j < len; j++) {
@@ -74,15 +94,15 @@ void BFSD(int v, int len, int* dist, int** G) {
 		q.pop();
 		cout << v + 1 << " ";
 		for (int i = 0; i < len; i++) {
-			if (G[v][i] == 1 && dist[i] == -1) {
+			if (G[v][i] != 0 && dist[i] == INT_MAX) {
 				q.push(i);
-				dist[i] = dist[v] + 1;
+				dist[i] = dist[v] + G[v][i];
 			}
 		}
 	}
 }
 
-void BFSD_list(int v, int len, int* dist, struct list * l) {
+void BFSD_list(int v, int len, int* dist, struct list* l) {
 	queue<int> q;
 	q.push(v);
 	dist[v] = 0;
@@ -91,8 +111,8 @@ void BFSD_list(int v, int len, int* dist, struct list * l) {
 		v = q.front();
 		q.pop();
 		cout << v + 1 << " ";
-		while(p){
-			if (dist[p->n - 1] == -1) {
+		while (p) {
+			if (dist[p->n - 1] == INT_MAX) {
 				q.push(p->n - 1);
 				dist[p->n - 1] = dist[v] + 1;
 			}
@@ -103,20 +123,22 @@ void BFSD_list(int v, int len, int* dist, struct list * l) {
 
 int main() {
 	setlocale(LC_ALL, "");
-	int** G, len, *dist, start;
+	int** G, len, * dist, start;
 	cout << "Введите длину графа: ";
 	cin >> len;
-	G = creatG(len);
+	G = creatG_orint(len);
 	printG(G, len);
 	dist = new int[len];
-	for (int i = 0; i < len; i++) dist[i] = -1;
+	for (int i = 0; i < len; i++) dist[i] = INT_MAX;
 	cout << "Введите вершину с которой начать обход: ";
 	cin >> start;
 	BFSD(start - 1, len, dist, G);
-	cout << "\n";
+	cout << "\nРастояние до вершин:\n";
 	for (int i = 0; i < len; i++) {
-		cout << dist[i] << " ";
-		dist[i] = -1;
+		cout << i + 1 << ": ";
+		if (dist[i] == INT_MAX) cout << "пути нет\n";
+		else cout << dist[i] << "\n";
+		dist[i] = INT_MAX;
 	}
 	list* l = new list[len];
 	cout << "\n\n";
@@ -130,8 +152,9 @@ int main() {
 	BFSD_list(start - 1, len, dist, l);
 	cout << "\n";
 	for (int i = 0; i < len; i++) {
-		cout << dist[i] << " ";
-		dist[i] = -1;
+		if (dist[i] == INT_MAX) cout << " пути нет ";
+		else cout << dist[i] << " ";
+		dist[i] = INT_MAX;
 	}
 	return 0;
 }
